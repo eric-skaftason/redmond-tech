45e36edbf73aa1d6790c7d285adbfabf99ddc4ea
sudo75 <144061928+sudo75@users.noreply.github.com>
2025-11-14 20:01:26 -0500

    Feature/dropdown (#13)
    * removed lorem ipsum text
    
    * add tech logo
    
    * dark theme
    
    * add blue accent
    
    * add blue to nav bar
    
    * added dropdown menu to nav bar
    
    ---------
    
    Co-authored-by: Father John Redmond Tech Team <redmondtechteam@gmail.com>

--- frontend/components/nav_bar/nav_bar.js
+++ frontend/components/nav_bar/nav_bar.js
@@ -21,10 +21,19 @@ class NavBar extends HTMLElement {
 
     // when element is added to DOM
     async connectedCallback() {
-        const response = await fetch('/components/nav_bar.html');
+        const response = await fetch('/components/nav_bar/nav_bar.html');
         const html = await response.text(); // use text method for HTML
-
         this.shadowRoot.innerHTML = html;
+
+        // Once this js file loads, the HTML dropdown elements get updated
+        await import("/components/dropdown/dropdown.js");
+
+        // Click on main bar to return to home
+        this.shadowRoot.addEventListener('click', (event) => {
+            const target = event.target;
+
+            if (target.id === 'title' || target.id === 'logo') document.location = '/';
+        });
     }
 
     // when element is removed from DOM
