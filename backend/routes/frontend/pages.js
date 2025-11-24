211e36c51626461ff4a3e0f6e4dad90f7a865157
Eric Skaftason <skaftasoneric@gmail.com>
2025-11-23 23:15:40 -0500

    reworked frontend file/folder structure to keep permissions consistent

--- backend/routes/frontend/pages.js
+++ backend/routes/frontend/pages.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const auth = require('../../middleware/auth.js');
 
-// Exception routes here:
+router.use('/public', express.static(path.join(__dirname, '../../../frontend/pages/public')));
+
+// HOME PAGE
 router.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../../frontend/pages/home/index.html'));
+    res.sendFile(path.resolve(__dirname, '../../../frontend/pages/public/home/index.html'));
 });
 
+// LOG IN
+router.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../../frontend/pages/public/login/login.html'))
+});
 
+// All routes after this require an account
+router.use(auth(0)); // must be suspended or higher
+
+// Account folder
 router.get('/account/dashboard', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../../frontend/pages/account/dashboard.html'))
+    res.sendFile(path.resolve(__dirname, '../../../frontend/pages/account/dashboard/dashboard.html'))
+});
+router.get('/account/logout', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../../frontend/pages/account/logout/logout.html'))
+});
+router.get('/account/sign_up', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../../frontend/pages/account/sign_up/sign_up.html'))
 });
 
 
