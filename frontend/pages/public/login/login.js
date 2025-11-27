40cb500580152db2009a61dcbbde1367a5ee89f7
ThePr0tagonist <jasonzwu.347@gmail.com>
2025-11-26 23:04:45 -0500

    try to add login check (dose not work)

--- frontend/pages/public/login/login.js
+++ frontend/pages/public/login/login.js
@@ -3,7 +3,7 @@ function togglePassword() {
     pwd.type = (pwd.type === 'password') ? 'text' : 'password';
 }
 
-function login() {
+function login2() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const errorMsg = document.getElementById('errorMsg');
@@ -22,4 +22,17 @@ function login() {
     */
 
     
+}
+async function logincheck(username, password){
+    const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+
+        body: JSON.stringify({username: username, password: password})
+    });
+
+    const data = await response.json();
 }
