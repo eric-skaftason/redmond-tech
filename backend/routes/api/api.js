075aa9d2470d086a8c68d55eaac9c7bde43ea6e4
Eric Skaftason <skaftasoneric@gmail.com>
2025-12-07 23:22:17 -0500

    added version numbers to the nav bar, updated app version

--- backend/routes/api/api.js
+++ backend/routes/api/api.js
@@ -7,4 +7,10 @@ router.use('/accounts', accounts);
 const file_sharing = require('./file_sharing.js');
 router.use('/file_sharing', file_sharing);
 
+const getAppInfo = require('../../utils/misc/app_info.js');
+router.get('/app_info', (req, res) => {
+    const app_info = getAppInfo();
+    res.status(200).json({ message: "Data retrieved", data: app_info });
+});
+
 module.exports = router;
