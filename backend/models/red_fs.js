96a0c61c9f7d7b5a904e60e4619c690138ada7fd
Eric Skaftason <skaftasoneric@gmail.com>
2025-12-24 12:22:19 -0500

    fixed added permissions data to folders

--- backend/models/red_fs.js
+++ backend/models/red_fs.js
@@ -4,9 +4,15 @@ const db = require('../config/db.js');
 
 class RedFS {
     static async createFolder(parent_folder_id, owner_id, folder_name, path) {
-        const sql = `INSERT INTO folders (parent_folder_id, owner_id, folder_name, path) VALUES (?, ?, ?, ?)`;
+        const sql_folder = `INSERT INTO folders (parent_folder_id, owner_id, folder_name, path) VALUES (?, ?, ?, ?)`;
+        
+        const [result] = await db.query(sql_folder, [parent_folder_id, owner_id, folder_name, path]);
+        // equivalent to const result = response[0];
 
-        await db.query(sql, [parent_folder_id, owner_id, folder_name, path]);
+        const folder_id = result.folder_id;
+
+        const sql_folder_permissions = `INSERT INTO folders_permissions (folder_id, account_id, permission_level) VALUES (?, ?, ?)`;
+        await db.query(sql_folder_permissions, [folder_id, owner_id, 4]);
         
     }
 
