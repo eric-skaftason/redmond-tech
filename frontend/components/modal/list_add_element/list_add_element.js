5b3674b0574b1892c28bd41f844037acbd6bb99d
Eric Skaftason <skaftasoneric@gmail.com>
2026-01-12 21:53:50 -0500

    greatly improved element addition ux

--- frontend/components/modal/list_add_element/list_add_element.js
+++ frontend/components/modal/list_add_element/list_add_element.js
@@ -24,18 +24,30 @@ export class ListAddElement extends HTMLElement {
         const text_input = this.shadowRoot.querySelector('.text_input');
         text_input.placeholder = text;
 
-        this.shadowRoot.querySelector('#add').addEventListener('click', async (event) => {
-            // Add element
-            const newElementText = this.shadowRoot.querySelector('.text_input').value;
-            const newElement = await this.shadowRoot.host.closest('list-input').appendElement(newElementText);
+        // running obj.method in an event listener doesn't work becasue it calls method instead of obj.method
+        // method must be bound to the object manually
+        this.shadowRoot.querySelector('#add').addEventListener('click', this.addElement.bind(this));
+        this.shadowRoot.querySelector('#text_input_add').addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') this.addElement();
+        });
+    }
 
-            // Scroll to bottom of body
+    async addElement() {
+        // Add element
+        const newElementText = this.shadowRoot.querySelector('.text_input').value;
+        const newElement = await this.shadowRoot.host.closest('list-input').appendElement(newElementText);
+
+        // if new element was created
+        if (newElement) {
+            // Scroll to bottom of body 
             setTimeout(() => {
                 // this.shadowRoot.host.closest('menu-body').scrollToEnd();
                 newElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
             }, 50);
-        
-        });
+
+            // clear input box
+            this.shadowRoot.querySelector('#text_input_add').value = '';
+        }
     }
 }
 customElements.define('list-add-element', ListAddElement);
