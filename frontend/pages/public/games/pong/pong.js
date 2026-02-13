99a34f27106e7bafcbe992e0713ed9d2a3947def
ThePr0tagonist <jasonzwu.347@gmail.com>
2026-02-12 20:42:07 -0500

    Update pong.js

--- frontend/pages/public/games/pong/pong.js
+++ frontend/pages/public/games/pong/pong.js
@@ -1,13 +1,13 @@
 //canvas setup
 let pongCanvas;
-let canvasWidth = 500;
+let canvasWidth = 700;
 let canvasHeight = 500;
 let context;
 
 //pong paddle 
 let paddleWidth = 10;
-let paddleHeight = 50;
-let paddleVolocityY =0;
+let paddleHeight = 60;
+let paddleVelocityY = 0;
 
 //ball
 let ballHeight = 10;
@@ -17,8 +17,8 @@ let ball = {
     y: canvasHeight/2 - ballHeight/2,
     width: ballWidth,
     height: ballHeight,
-    volocityX : 1,
-    volocityY : 2,
+    velocityX : 2,
+    velocityY : 2,
 };
 
 //player 
@@ -27,14 +27,14 @@ let player1 = {
     y : canvasHeight/2 - paddleHeight/2,
     width : paddleWidth,
     height : paddleHeight,
-    volocityY : paddleVolocityY,
+    velocityY : paddleVelocityY,
 };
 let player2 = {
     x : canvasWidth - paddleWidth -10,
     y : canvasHeight/2 - paddleHeight/2,
     width : paddleWidth,
     height : paddleHeight,
-    volocityY : paddleVolocityY,
+    velocityY : paddleVelocityY,
 }
 
 //player points
@@ -86,47 +86,70 @@ function update(){
     requestAnimationFrame(update);
     context.clearRect(0, 0, canvasWidth, canvasHeight);
     //p1
-    let newPlayer1Y = player1.y + player1.volocityY;
+    let newPlayer1Y = player1.y + player1.velocityY;
     if (!outofBounds(newPlayer1Y)) {
         player1.y = newPlayer1Y;
     }
     context.fillRect(player1.x, player1.y, player1.width, player1.height);
 
     //p2    
-    let newPlayer2Y = player2.y + player2.volocityY;
+    let newPlayer2Y = player2.y + player2.velocityY;
     if (!outofBounds(newPlayer2Y)) {
         player2.y = newPlayer2Y;
     }
     context.fillRect(player2.x, player2.y, player2.width, player2.height);
     //ball
     context.fillStyle = "#FFFFFF";
-    ball.x += ball.volocityX;
-    ball.y += ball.volocityY;
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
     context.fillRect(ball.x, ball.y, ball.width, ball.height);
         if(ball.y <=0 || ball.y + ball.height >= canvasHeight){
-            ball.volocityY *= -1;}
+            ball.velocityY *= -1;
+        }
         if (colisionDetection(ball, player1) || colisionDetection(ball, player2)){
-            ball.volocityX *= -1;}
+            ball.velocityX *= -1;
+  
+            let hitPaddle = colisionDetection(ball, player1) ? player1 : player2;
+            let hitPos = (ball.y - hitPaddle.y) / hitPaddle.height;
+            ball.velocityY = (hitPos - 0.5) * 6;
 
+            ball.velocityY += hitPaddle.velocityY * 0.3;
+
+            ball.velocityX = (ball.velocityX > 0 ? 1 : -1) * Math.min(4, Math.abs(ball.velocityX) + 0.2);
+            ball.velocityY = Math.max(-4, Math.min(4, ball.velocityY));
+        }
+            //game over
+            if(ball.x<0){
+                player2Points++;
+                gameReset(1);
+            }   
+            else if(ball.x+ballWidth>canvasWidth){
+                player1Points++;
+                gameReset(-1)
+            }
+            //score
+            context.font="45px sans-sarif"
+            context.fillText(player1Points,canvasWidth/5,45);
+            context.fillText(player2Points,canvasWidth*4/5-45,45);
 }
 
 function updateVelocities(e){
-    player1.volocityY = 0;
-    player2.volocityY = 0;
+    player1.velocityY = 0;
+    player2.velocityY = 0;
     //p1
     if (heldKeys.w){
-        player1.volocityY = -3;
+        player1.velocityY = -3;
     }
     else if (heldKeys.s){
-        player1.volocityY = 3;
+        player1.velocityY = 3;
     }
 
     //p2
     if (heldKeys.ArrowUp){
-        player2.volocityY = -3;
+        player2.velocityY = -3;
     }
     else if (heldKeys.ArrowDown){
-        player2.volocityY = 3;
+        player2.velocityY = 3;
     }
 }
 
@@ -136,3 +159,12 @@ function colisionDetection(a, b){
             a.y < b.y + b.height &&
             a.y + a.height > b.y);
 }
+
+function gameReset(direction){
+
+    ball.x = canvasWidth/2 - ball.width/2;
+    ball.y = canvasHeight/2 - ball.height/2;
+    ball.velocityX = direction * 2;
+    ball.velocityY = (Math.random() - 0.5) * 4;
+};
+
